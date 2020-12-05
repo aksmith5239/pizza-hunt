f8e8a131f6b9d11c748ee4b45d7945b5cc21874e
aksmith5239 <aklobby@gmail.com>
2020-12-05 11:41:13 -0700

    updated delete comment controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -37,27 +37,28 @@ const commentController = {
         .catch(err => res.json(err));
     },
     //remove comment
-    removeComment({params} , res){
-        Comment.findOneAndDelete({_id: params.commentId})
-        .then(deletedComment => {
-            if(!deletedComment) {
-                return res.status(404).json({message: "We could not find the comment with this id!"});
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+          .then(deletedComment => {
+            if (!deletedComment) {
+              return res.status(404).json({ message: 'No comment with this id!' });
             }
             return Pizza.findOneAndUpdate(
-                {_id: params.pizzaId},
-                {$pull: {comments: params.commentId}},
-                {new: true}
+              { _id: params.pizzaId },
+              { $pull: { comments: params.commentId } },
+              { new: true }
             );
-        })
-        .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({message: "We could not find the pizza you are looking for!"});
-                return;
+          })
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
             }
             res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
-    },
+          })
+          .catch(err => res.json(err));
+      },
+    
     removeReply({params}, res) {
         Comment.findOneAndUpdate(
                 {_id: params.pizzaId},
